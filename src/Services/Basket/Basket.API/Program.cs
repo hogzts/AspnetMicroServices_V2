d52694dd4c0e319db33ef382b9e98c7828294326
Hüseyin Ogzts <oguztas@gmail.com>
2023-04-04 02:27:46 +0300

    commiting basket api

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//added Redis configuration - huseyino
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+//
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//added interfaces registration - huseyino
+//builder.Services.AddStackExchangeRedisCache
+
+//
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
